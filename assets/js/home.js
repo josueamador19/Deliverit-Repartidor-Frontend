a49273935e3be7f46deb76a8dd869547e22711df
Amador19 <67528706+josueamador19@users.noreply.github.com>
2022-10-21 16:28:33 -0600

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,11 +1,24 @@
 
+const list=document.querySelectorAll('.list');
+function activeLink(){
+    list.forEach((item)=> item.classList.remove('active'));
+    this.classList.add('active');
 
-//install SW
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-      navigator.serviceWorker
-        .register("/sw.js")
-        .then(res => console.log("service worker registered"))
-        .catch(err => console.log("service worker not registered", err));
-    });
+}
+list.forEach((item)=>item.addEventListener('click', activeLink));
+
+function assignedClick(){
+    document.getElementById('orders-assigned').style.display ='block';
+    document.getElementById('orders-in-progress').style.display ='none';
+    document.getElementById('orders-completed').style.display ='none';
+}
+function progressClick(){
+    document.getElementById('orders-assigned').style.display ='none';
+    document.getElementById('orders-in-progress').style.display ='block';
+    document.getElementById('orders-completed').style.display ='none';
+}
+function completedClick(){
+    document.getElementById('orders-assigned').style.display ='none';
+    document.getElementById('orders-in-progress').style.display ='none';
+    document.getElementById('orders-completed').style.display ='block';
 }
