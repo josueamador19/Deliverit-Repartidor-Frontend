4c97a2cace44ef3dd0de02d75875993f8024d9d9
josueamador19 <amadoroscar789@gmail.com>
2022-11-11 14:11:34 -0600

    validaciones

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,13 +2,9 @@ function loginClick() {
    validate();
 }
 
-function validate(){
-    let username= document.getElementById('username').value;
-    let password= document.getElementById('password').value;
-
-    if(username ==='test1' && password==='1234'){
-        alert('Login succesfuly!');
-    }else{
-        alert('Login failed!');
-    }
-}
+const name= document.getElementById('username').value;
+const password= document.getElementById('password').value;
+const form=document.getElementById('form')
+form.addEventListener("submit",e=>{
+    
+})
---
+++ assets/js/register.js
@@ -0,0 +1,28 @@
+
+const nombre= document.getElementById('username');
+const password= document.getElementById('password');
+const email= document.getElementById('email');
+const phoneNumber= document.getElementById('phoneNumber');
+const form=document.getElementById('form')
+form.addEventListener("submit",e=>{
+    e.preventDefault()
+    let entrar=false
+    let regexEmail=/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    if(nombre.value.length<6){
+        alert("Nombre muy corto");
+        entrar=true
+        
+    }
+    if(!regexEmail.test(email.value)){
+        alert("El email no es valido");
+        entrar=true
+    }
+    if(phoneNumber.value.length!==8){
+        alert("El campo debe de tener 8 digitos")
+    }
+    if(password.value.length<8){
+        alert("La contraseña no es valida");
+        entrar=true
+    }
+    
+})
