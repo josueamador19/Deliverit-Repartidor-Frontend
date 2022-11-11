09099fd6c2dcd50ac6cbfc32c0170fd140b57d94
josueamador19 <amadoroscar789@gmail.com>
2022-11-11 16:02:40 -0600

    validaciones

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,7 +3,7 @@ const nombre= document.getElementById('username');
 const password= document.getElementById('password');
 const form=document.getElementById('form')
 form.addEventListener("submit",e=>{
-    if(nombre.value==="amador" && password.value==="1234"){
+    if(nombre.value==="admin" && password.value==="admin"){
       windows.location.href('home.html');
     }else{
       alert("Usuario invalido")
