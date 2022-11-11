18a4f1f58856d100477caa1227118263fb8a8724
josueamador19 <amadoroscar789@gmail.com>
2022-11-11 13:07:12 -0600

    validaciones

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,14 @@
 function loginClick() {
-    
-    window.location.href = 'home.html';
+   validate();
+}
+
+function validate(){
+    let username= document.getElementById('username').value;
+    let password= document.getElementById('password').value;
+
+    if(username ==='test1' && password==='1234'){
+        alert('Login succesfuly!');
+    }else{
+        alert('Login failed!');
+    }
 }
