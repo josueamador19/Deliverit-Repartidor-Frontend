109377ced90b41431d8e92685ece92b998ed788a
josueamador19 <amadoroscar789@gmail.com>
2022-11-12 17:40:42 -0600

    ediciones

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,13 +1,30 @@
+let users=[
+  {
+    name:'admin',
+    pass:'admin'
+  }
+]
 
 const nombre= document.getElementById('username');
 const password= document.getElementById('password');
 const form=document.getElementById('form')
 
+
 form.addEventListener("submit",e=>{
-    e.preventDefault();
+  e.preventDefault();
+  users.forEach(user => {
+    if(nombre.value===user.name && password.value===user.pass){
+      window.location.href='home.html';
+    }else{
+      alert("Usuario Invalido");
+    }
+  });
+  /*
+    
     if(nombre.value==="admin" && password.value==="admin"){
       window.location.href = 'home.html';
     }else{
       alert("Usuario invalido")
     }
+    */
 })
